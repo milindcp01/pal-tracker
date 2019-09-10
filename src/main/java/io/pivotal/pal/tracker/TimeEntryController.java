8f2fb684326c5431e5d5e05a0b5b0b7bfa018ca9
training <milindcp01@gmail.com>
2019-09-10 16:44:49 -0700

    4th LAB assignment

---
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -0,0 +1,57 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Objects;
+
+@RestController
+@RequestMapping("/time-entries")
+public class TimeEntryController {
+
+
+    private TimeEntryRepository timeEntryRepository;
+
+
+    public TimeEntryController(TimeEntryRepository timeEntryRepository) {
+        this.timeEntryRepository = timeEntryRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity create(@RequestBody TimeEntry timeEntry) {
+        return new ResponseEntity<>(timeEntryRepository.create(timeEntry),HttpStatus.CREATED);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<TimeEntry> read(@PathVariable long id) {
+        TimeEntry timeEntry = timeEntryRepository.find(id);
+        if(Objects.nonNull(timeEntry))
+            return new ResponseEntity<>(timeEntry,HttpStatus.OK);
+        else
+        return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TimeEntry>> list() {
+        return new ResponseEntity<>(timeEntryRepository.list(),HttpStatus.OK);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<TimeEntry> update(@PathVariable long id,@RequestBody TimeEntry timeEntry) {
+        TimeEntry updatedTimeEntry = timeEntryRepository.update(id, timeEntry);
+        if (updatedTimeEntry != null) {
+            return new ResponseEntity<>(updatedTimeEntry, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity delete(@PathVariable long id) {
+        timeEntryRepository.delete(id);
+        return new ResponseEntity<>("",HttpStatus.OK);
+    }
+
+}
